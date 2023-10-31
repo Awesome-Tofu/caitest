43167ac6b211141380070bc500a13ee3e37dfd9c
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-10-31 18:12:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ app.get("/", (req, res) => {
   const characterId = req.query.id;
   const message = req.query.teks;
   const accessToken = req.query.token;
+  // const accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkVqYmxXUlVCWERJX0dDOTJCa2N1YyJ9.eyJpc3MiOiJodHRwczovL2NoYXJhY3Rlci1haS51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjUxNmU5MzE4MDY0MGRjZjc1MGNjZjZjIiwiYXVkIjpbImh0dHBzOi8vYXV0aDAuY2hhcmFjdGVyLmFpLyIsImh0dHBzOi8vY2hhcmFjdGVyLWFpLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2OTg3NTUwMzEsImV4cCI6MTcwMTM0NzAzMSwiYXpwIjoiZHlEM2dFMjgxTXFnSVNHN0Z1SVhZaEwyV0VrbnFaenYiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.A0pZMo-u8IUha7Nxn7a7NpaZF8xpfOLa99ci1MS19oKIc6PDoclbGYntRga45zed094UlADZpdy7qlNyAEVQI572w2iuE4N7K_O9IM2cIEVU3qTrtSSPvT_OCva_vteMtqJlJG0Wc5C6Cx2rlcXS6eJDYVcHZYLpL8l-jLSXjbVjI3PWWXkrEvfTIwS5xzojYxOiEEFGUSJMokoBPtfBcec8UaMPh9UNogLRLOJO2_ieWt94gYCL9YbHx7ItT89qyFYB9EWiEKMyAEPmG8qwa09XuY9FOAoZxLwIYKs3vTAm1cvojsXtxIOE1bU_bUwccBlWcGugpgyLZtPKOgy2uA";
 
   queue.push({ characterId, message, accessToken, res }, (error) => {
     if (error) {
